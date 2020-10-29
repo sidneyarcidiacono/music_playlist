a2f2ff16cc46bf799fe5f9ef1edd0938ef10ee09
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-10-29 13:16:33 -0700

    modified to doubly linked list, included delete from head and delete from tail methods.

--- linked_list.py
+++ linked_list.py
@@ -15,6 +15,7 @@ class LinkedList:
         node = Node(data)
         if self.tail is not None:
             self.tail.next = node
+            node.previous = self.tail
         self.tail = node
         if self.head is None:
             self.head = node
@@ -23,7 +24,26 @@ class LinkedList:
         """Create a new Node and prepend to list."""
         node = Node(data)
         if self.head is not None:
+            self.head.previous = node
             node.next = self.head
         self.head = node
         if self.tail is None:
             self.tail = node
+            self.tail.next = None
+            self.tail.previous = None
+
+    def print_songs(self):
+        """Print all 'songs' (nodes) in list."""
+        current = self.head
+        while current is not None:
+            print(current.data)
+            current = self.next
+
+    def delete_from_head(self):
+        """Delete item from head."""
+        self.head = self.head.next
+
+    def delete_from_tail(self):
+        """Delete item from tail."""
+        self.tail.previous = self.tail
+        self.tail.next = None
--- node.py
+++ node.py
@@ -5,5 +5,7 @@ class Node:
     """Define Node class to track node properties."""
 
     def __init__(self, data):
+        """Set Node properties."""
+        self.data = data
         self.next = None
         self.previous = None
