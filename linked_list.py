ecab4fb70cf05a6cfb55877d41a115574151afa9
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-10-29 16:59:03 -0700

    Finished all challenges

--- linked_list.py
+++ linked_list.py
@@ -16,9 +16,11 @@ class LinkedList:
         if self.tail is not None:
             self.tail.next = node
             node.previous = self.tail
-        self.tail = node
+            node.next = None
+            self.tail = node
         if self.head is None:
             self.head = node
+        self.tail = node
 
     def prepend(self, data):
         """Create a new Node and prepend to list."""
@@ -26,18 +28,19 @@ class LinkedList:
         if self.head is not None:
             self.head.previous = node
             node.next = self.head
-        self.head = node
         if self.tail is None:
             self.tail = node
             self.tail.next = None
             self.tail.previous = None
+        self.head = node
+        self.head.previous = None
 
     def print_songs(self):
         """Print all 'songs' (nodes) in list."""
         current = self.head
         while current is not None:
             print(current.data)
-            current = self.next
+            current = current.next
 
     def delete_from_head(self):
         """Delete item from head."""
@@ -45,15 +48,47 @@ class LinkedList:
 
     def delete_from_tail(self):
         """Delete item from tail."""
-        self.tail.previous = self.tail
+        self.tail = self.tail.previous
         self.tail.next = None
+
+    def delete(self, item):
+        """Delete item from anywhere in list."""
+        current = self.head
+        while current is not None:
+            if current.data is item:
+                if current.next is not None and current.previous is not None:
+                    current = current.previous
+                    current.next = current.next.next
+                elif current.next.next is None:
+                    current = current.previous
+                    current.next = None
+                    self.tail = current
+                elif current.previous is None:
+                    self.head = current.next
+                    self.head.next = current.next.next
+                break
+            else:
+                current = current.next
 
     def find(self, item):
         """Find specific item in list."""
         current = self.head
+        found = False
         while current is not None:
-            if current == item:
-                return True
-            elif current != item:
+            if current.data is not item:
                 current = current.next
-        return False
+            elif current.data is item:
+                found = True
+            return found
+
+    def reverse(self):
+        """Reverse order of list."""
+        next = None
+        prev = None
+        current = self.head
+        while current is not None:
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        self.head = prev
--- main.py
+++ main.py
@@ -1,2 +1,32 @@
 """Import class modules."""
 from linked_list import LinkedList
+
+playlist = LinkedList()
+
+playlist.append('The Payback')
+playlist.prepend('Mutant')
+playlist.append('Xen')
+playlist.append('To Live and Die in LA')
+playlist.append('Stop Trying to be God')
+playlist.prepend('I Love You')
+print("PRINTING ORIGINAL PLAYLIST")
+playlist.print_songs()
+
+playlist.delete_from_head()
+print("PRINTING AFTER DELETING FROM HEAD")
+playlist.print_songs()
+
+playlist.delete_from_tail()
+print("PRINTING AFTER DELETING FROM TAIL")
+playlist.print_songs()
+
+print("FIND MUTANT")
+print(playlist.find('Mutant'))
+
+playlist.delete('Mutant')
+print("PRINT AFTER DELETING MUTANT")
+playlist.print_songs()
+
+playlist.reverse()
+print("AFTER REVERSE")
+playlist.print_songs()
